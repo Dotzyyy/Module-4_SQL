526f04331b991617fe8fcd3a9728db04fe91cd71
David Sexton <davidsexton36@gmail.com>
2024-03-30 17:29:32 +0000

    database connected

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-from app import models
+from models import *
 
 if __name__ == '__main__':
         app.run(debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    SQLALCHEMY_DATABASE_URI = "postgresql://david:password@localhost:5432/flask_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/flask_db"
     
