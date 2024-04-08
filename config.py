e6cfe2c2f1070118a46eb2344cb9b0a59c2f0fd2
David Sexton <davidsexton36@gmail.com>
2024-04-08 23:48:43 +0100

    grdgdr

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
 import os
-
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
     
     
-    default_connection_string = "postgresql://postgres:password@localhost:5432/flask_db"
-    
     if os.environ.get('RENDER'):
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://david_sexton:xvzBRkawcGGQAdFiiqxjqVoHJpigL33U@dpg-coa5stsf7o1s73dku450-a.frankfurt-postgres.render.com/flask_db_9euf')
-
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://david_sexton:xvzBRkawcGGQAdFiiqxjqVoHJpigL33U@dpg-coa5stsf7o1s73dku450-a.frankfurt-postgres.render.com/flask_db_9euf' )
+    
     else:
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', default_connection_string )
-
-        
+        SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') or "postgresql://postgres:password@localhost:5432/flask_db"
