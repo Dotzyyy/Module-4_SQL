2420a8c42e30168aa412e7673a23145c852cfef7
David Sexton <davidsexton36@gmail.com>
2024-04-08 22:04:57 +0100

    config edit

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
+from sqlalchemy import create_engine
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/flask_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.getenv("postgresql://postgres:password@localhost:5432/flask_db")
     
     if os.environ.get('RENDER'):
-        connection_string = os.environ.get('DATABASE_URL', 'postgres://flask2_db_user:fUIr43Xuux1HCZmOGyD7V1GFb2etJpy9@dpg-coa4a5i1hbls73fjajlg-a/flask2_db' )
+        connection_string = os.environ.get('DATABASE_URL', 'postgresql://flask2_db_user:fUIr43Xuux1HCZmOGyD7V1GFb2etJpy9@dpg-coa4a5i1hbls73fjajlg-a/flask2_db' )
 
     else:
         connection_string = os.environ.get('DATABASE_URL', SQLALCHEMY_DATABASE_URI )
+
+        
