0b3bc27b7d0ce2336fca0a9a852d47409b008837
David Sexton <davidsexton36@gmail.com>
2024-04-09 00:02:23 +0100

    possible help

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/flask_db"
     
     if os.environ.get('RENDER'):
-        connection_string = os.environ.get('DATABASE_URL', 'postgres://flask2_db_user:fUIr43Xuux1HCZmOGyD7V1GFb2etJpy9@dpg-coa4a5i1hbls73fjajlg-a.frankfurt-postgres.render.com/flask2_db' )
-    
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgres://flask2_db_user:fUIr43Xuux1HCZmOGyD7V1GFb2etJpy9@dpg-coa4a5i1hbls73fjajlg-a.frankfurt-postgres.render.com/flask2_db')
     else:
-        connection_string = os.environ.get
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "postgresql://postgres:password@localhost:5432/flask_db")
