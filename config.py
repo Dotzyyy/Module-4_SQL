f4d769231cee85a9f0823fc2a1184dcfec6aea53
David Sexton <davidsexton36@gmail.com>
2024-04-09 00:20:27 +0100

    dqqwdqd

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
 from sqlalchemy import *
-
 class Config:
-
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    
+    SQLALCHEMY_DATABASE_URI = "DATABASE_URL"
     
     if os.environ.get('RENDER'):
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+        connection_string = os.environ.get('DATABASE_URL')
+    
     else:
-        # Use the local database URL if RENDER environment variable is not set
-        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/flask_db"
+        connection_string = os.environ.get('DATABASE_URL', "postgresql://postgres:password@localhost:5432/flask_db" )
