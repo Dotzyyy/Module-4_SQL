9e38088c2286ad311ae7598b5056d4e54b95ba51
David Sexton <davidsexton36@gmail.com>
2024-05-13 12:55:13 +0100

    db_uri

--- config.py
+++ config.py
@@ -5,14 +5,15 @@ class Config:
     if 'RENDER' in os.environ:
         # Running on Render
         db_uri = os.environ['DATABASE_URL']
+        SQLALCHEMY_DATABASE_URI = db_uri
     else:
     # Local development
         db_uri = 'postgresql://postgres:password@localhost:5432/flask_db'
-
+        SQLALCHEMY_DATABASE_URI = db_uri
 
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     
