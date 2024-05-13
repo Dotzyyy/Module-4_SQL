b750fe7b4df1da56024c76b6a0f5be628d045c7c
David Sexton <davidsexton36@gmail.com>
2024-05-13 11:04:25 +0100

    render

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://david_sexton:xvzBRkawcGGQAdFiiqxjqVoHJpigL33U@dpg-coa5stsf7o1s73dku450-a/flask_db_9euf")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     
     
