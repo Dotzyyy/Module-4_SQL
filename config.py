803eebb26c35671e4590422a4c2986136f4121ea
David Sexton <davidsexton36@gmail.com>
2024-06-09 13:58:53 +0100

    Database render fix part 100

--- config.py
+++ config.py
@@ -1,7 +1,16 @@
 import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/flask_db"
+    
+    if os.environ.get('ENV') == 'local':
+
+        SQLALCHEMY_DATABASE_URI = os.environ.get('LOCAL_DATABASE_URL')
+    
+    else:
+
+        SQLALCHEMY_DATABASE_URI = os.environ.get('RENDER_DATABASE_URL')
+
+    
 
     
     
