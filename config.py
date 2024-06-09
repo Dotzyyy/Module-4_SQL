df19e00b0407dfa712ddbebcdc93a65baa175af3
David Sexton <davidsexton36@gmail.com>
2024-06-09 15:17:05 +0100

    render variable

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '961fc2bb17a331c5c14847e730850e4d'
     
-    if os.environ.get('RENDER') == 'local':
+    if os.environ.get('RENDER'):
         
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://flask_db_d6ml_user:DlJLp9X8q21fl9RBoZP8Y5zkTXDdgVVk@dpg-cpiq6oi1hbls73bl78j0-a/flask_db_d6ml')
         
